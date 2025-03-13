fc5d5f014d15bc5b3700695d01743020d58e2cf7
1909873 <1909873@01-18113.pusd.dom>
2025-03-13 13:16:35 -0700

    made test case with multiple rooms, need to fix map reading for multiplerooms

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -7,7 +7,7 @@ public class p2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("p2");
-		readMap("TEST01");
+		readMap("TEST04");
 		
 		
 	}
