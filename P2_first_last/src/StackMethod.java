a4a9044c24b43c2db30ba91ac27e604e6eb4ff3a
shaynamehta <shaynamehta01@gmail.com>
2025-03-26 23:46:58 -0700

    idk if this is properly commit and pushing

---
+++ P2_first_last/src/StackMethod.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class StackMethod {
+	
+	//add variables here and initialize later
+	int r; 
+	int c; 
+	int n; 
+	
+	//locatin of W
+	int rW;
+	int cW; 
+	int nW;
+	
+	//create vars for stacks and map
+	private Stack<Position> ogTBV;
+	private Stack<Position> revTBV;
+	private Stack<Position> visited;
+	private Stack<Position> revV;
+	private String[][][] map;
+	
+	
+
+}
